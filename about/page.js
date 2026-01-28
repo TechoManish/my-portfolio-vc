0593164d03c54872bd2bf8ef066c2929a2596bcc
TechoManish <98860618+TechoManish@users.noreply.github.com>
2026-01-28 13:05:56 +0530

    Create page.js

---
+++ about/page.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>About Me</h1>
+      <p>
+        I am learning web development and building projects using Next.js,
+        Vercel, and modern tools.
+      </p>
+    </main>
+  );
+}
